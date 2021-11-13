f43e4ab4c43958c6b9858f39f6a8a6bc1c37509b
Toktar <ufazak@gmail.com>
2021-11-13 11:36:22 +0600

    Загружаем mock данные в UserService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {User} from "../users/user.interface";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,13 @@ export class UserService {
 
   users: Array<User> = []
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.http.get("./assets/users.json")
+      .subscribe(data => {
+        // три точки распаковывают элементы массива
+        // далее они упаковываются в массив users
+        this.users.push(...<Array<User>> data)
+      })
   }
 
   add(user: User) {
